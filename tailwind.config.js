2f300bfc921aaa8426bd89145b421be00d917767
hello <hello@gmail.com>
2024-09-18 13:36:05 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+ 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "clersky-green": "#198C36",
+      "clersky-green-dark": "#163A24",
+      "clersky-white": "#FBFAF7",
+      "clersky-brown": "#FAB5A0",
+      "clersky-black": "#163A24",
+      "card-gray": "#FBFAF7",
+      "white-t-color": "#FBFAF7",
+    },
+    fonts: {
+      
+    }
+  },
+  plugins: [],
+};
